bd541d32397fd83ed663662bca26b6c0558fc71d
bilalikram94 <bilalikram94@gmail.com>
2018-05-18 10:10:54 +0500

    working
    updated

--- FindByClassTag.py
+++ FindByClassTag.py
@@ -11,10 +11,16 @@ class FindByClassTag:
     @staticmethod
     def test():
         driver.get(baseURL)
-        FindElementByXpath = driver.find_elements_by_xpath("//input[@id='displayed-text']")
-        FindElementByXpath
+        FindElementByClass = driver.find_element_by_class_name("displayed-class")
+        FindElementByClass.send_keys("element found")
         if FindElementByClass is not None:
             print("element found")
+
+        elementByTag = driver.find_element_by_tag_name('h1')
+        text = elementByTag.text
+        if elementByTag is not None:
+            print("element found by tag and the text on the element is: " + text)
+
 
 
 ff = FindByClassTag()
