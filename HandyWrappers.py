40e1a18cb4721deaff191a21763b8de9a3368469
bilalikram94 <bilalikram94@gmail.com>
2018-05-24 16:23:08 +0500

    updated

--- HandyWrappers.py
+++ HandyWrappers.py
@@ -1,40 +1,61 @@
-"""
-Dependencies UsingWrappers.py
-"""
 from selenium.webdriver.common.by import By
 
 class HandyWrappers():
+
     def __init__(self, driver):
         self.driver = driver
 
-    def getByType (self, locatorType):
+    def getByType(self, locatorType):
         locatorType = locatorType.lower()
         if locatorType == "id":
             return By.ID
+        elif locatorType == "name":
+            return By.NAME
         elif locatorType == "xpath":
             return By.XPATH
-        elif locatorType == "tag":
-            return By.TAG_NAME
-        elif locatorType == "link text":
-            return By.LINK_TEXT
-        elif locatorType == "class":
-            return By.CLASS_NAME
-        elif locatorType == "css selector":
+        elif locatorType == "css":
             return By.CSS_SELECTOR
-        elif locatorType == "partial link text":
-            return By.PARTIAL_LINK_TEXT
+        elif locatorType == "classname":
+            return By.CLASS_NAME
+        elif locatorType == "linktext":
+            return By.LINK_TEXT
         else:
-            print("Locator is invalid")
+            print("Locator type " + locatorType + " not correct/supported")
         return False
+
     def getElement(self, locator, locatorType="id"):
         element = None
         try:
             locatorType = locatorType.lower()
             byType = self.getByType(locatorType)
             element = self.driver.find_element(byType, locator)
-            print("element is found")
+            print("Element Found")
         except:
             print("Element not found")
         return element
 
+    def isElementPresent(self, locator, byType):
+        try:
+            element = self.driver.find_element(byType, locator)
+            if element is not None:
+                print("Element Found")
+                return True
+            else:
+                print("Element not found")
+                return False
+        except:
+            print("Element not found")
+            return False
 
+    def elementPresenceCheck(self, locator, byType):
+        try:
+            elementList = self.driver.find_elements(byType, locator)
+            if len(elementList) > 0:
+                print("Element Found")
+                return True
+            else:
+                print("Element not found")
+                return False
+        except:
+            print("Element not found")
+            return False
